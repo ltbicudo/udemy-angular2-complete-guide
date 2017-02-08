21cf18fd71f8ba7d4161e60ee7fc17632f53f7fe
Lucas Bicudo <Lucas.Bicudo@techsolcom.local>
2017-02-08 16:56:58 -0500

    Session 145 to 170

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+
+import 'rxjs/Rx';
 
 import { Recipe } from './recipe';
 import { Ingredient } from '../shared/ingredient';
@@ -12,7 +15,7 @@ export class RecipeService {
     new Recipe('Dummy 3', 'Dummy 3', 'https://www.iconexperience.com/_img/g_collection_png/standard/256x256/crash_test_dummy.png', [])
   ];
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getRecipes() {
     return this.recipes;
@@ -32,6 +35,24 @@ export class RecipeService {
 
   editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData() {
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return this.http.put('https://recipebook-aa497.firebaseio.com/recipes.json', body, {headers});
+  }
+
+  fetchData() {
+    return this.http.get('https://recipebook-aa497.firebaseio.com/recipes.json')
+      .map((response: Response) => response.json())
+      .subscribe(
+        (data: Recipe[]) => {
+          this.recipes = data;
+        }
+      );
   }
 
 }
